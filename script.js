155d3bdc2aec8a83d0955b2d8d8475ecc762f0a4
Man <inumerasnuvens@gmail.com>
2023-03-26 21:40:07 -0300

    if

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function toggleMode() {
     const img = document.querySelector("#profile img")
     
     //Substituir a img
-    if(html.classList.contains('light')) {
+    if (html.classList.contains('light')) {
 
     //Se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-dark.png")
